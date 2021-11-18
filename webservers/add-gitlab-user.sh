2ecbd6d6c0cc8fe9d673168a1bd3fbe240b3ccba
UnwishingMoon <11472973+UnwishingMoon@users.noreply.github.com>
2021-11-18 21:26:35 +0100

    Added passphrase to gitlab script

--- webservers/add-gitlab-user.sh
+++ webservers/add-gitlab-user.sh
@@ -9,8 +9,18 @@
 #                                                       #
 #########################################################
 
-# Creating gitlab user and .ssh files
+# Exit signal
+trap 'stty echo' INT
+
+# Asking for a passphrase
+stty -echo
+read -p "Enter the passphrase (leave blank to not set): " pass
+stty echo
+
+# Creating gitlab user
 adduser --disabled-password gitlab
+
+# Creating user directory and files
 mkdir -p /home/gitlab/.ssh
 touch /home/gitlab/.ssh/authorized_keys
 chown -R gitlab.gitlab /home/gitlab/.ssh/
@@ -18,15 +28,21 @@ chmod 700 /home/gitlab/.ssh/
 chmod 600 /home/gitlab/.ssh/authorized_keys
 
 # Generating SSH key for gitlab user - You should delete the keys after downloading them
-ssh-keygen -b 4096 -t rsa -m PEM -C "gitlab" -f "/root/gitlab-runner.pem" -N ""
+ssh-keygen -b 4096 -t rsa -m PEM -C "gitlab" -f "/root/gitlab-runner.pem" -N "$pass"
+
+# Copying pub key to authorized_keys
 cat /root/gitlab-runner.pem.pub > /home/gitlab/.ssh/authorized_keys
+
+# Securing keys
 chmod 600 /root/gitlab-runner.pem /root/gitlab-runner.pem.pub
 
 # Adding groups to users
-usermod -aG gitlab admin
-
-if [ $(getent group nginx)]; then
+if [ $(getent passwd admin) ]; then
+    usermod -aG gitlab admin
+fi
+if [ $(getent group nginx) ]; then
     usermod -aG nginx gitlab
 else
+if [ $(getent group www-data) ]; then
     usermod -aG www-data gitlab
 fi

fdebb52cb75ff8a9600fe45609f1ddec14bda41e
UnwishingMoon <11472973+UnwishingMoon@users.noreply.github.com>
2021-11-18 21:14:25 +0100

    Removed curl and added comments

--- webservers/update-aws-cli.sh
+++ webservers/update-aws-cli.sh
@@ -9,12 +9,23 @@
 #                                                       #
 #########################################################
 
-AWSCliUrl="https://www.diegocastagna.com/files/awscli.tar.xz"   # Just the awscli but with tar.xz compression
+# Just the awscli but with tar.xz compression
+AWSCliUrl="https://www.diegocastagna.com/files/awscli.tar.xz"
 
-# Updating AWS Cli
-apt -yq remove awscli
-wget -U "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36" -O /root/awscli.tar.xz "$AWSCliUrl"
+# Installing required packages
+apt-get update -yq
+apt-get install -yq curl
+
+# Removing distribution AWS Cli
+apt-get -yq remove awscli
+
+# Downloading the cli
+curl -o /root/awscli.tar.xz "$AWSCliUrl"
 tar -xf /root/awscli.tar.xz -C /root/
+
+# Installing the cli
 bash /root/aws/install
-rm -r /root/aws/
-rm /root/awscli.tar.xz
+
+# Removing temporary files
+rm -rf /root/aws/
+rm -f /root/awscli.tar.xz
